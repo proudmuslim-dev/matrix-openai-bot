f0b8070e2e6a5078df2e408c047d0c39d710d8ba
Proudmuslim <proudmuslimos@protonmail.ch>
2021-09-05 10:39:37 -0700

    Get started on properly parsing requests

--- src/openai.rs
+++ src/openai.rs
@@ -1,6 +1,19 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct GPTRequest {
+    prompt: String,
+    max_tokens: u16,
+    temperature: u8,
+    top_p: u8,
+    n: u8,
+    stream: bool,
+    logprobs: String,
+    stop: String,
+    echo: bool
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct GPTResponse {
     id: String,
     object: String,
@@ -15,6 +28,42 @@ struct Choices {
     index: u8,
     logprobs: String,
     finish_reason: String,
+}
+
+impl GPTRequest {
+    pub fn new() -> Self {
+        Self {
+            prompt: String::new(),
+            max_tokens: 16,
+            temperature: 1,
+            top_p: 1,
+            n: 1,
+            stream: false,
+            logprobs: "null".to_owned(),
+            stop: "\n".to_owned(),
+            echo: false
+        }
+    }
+
+    pub fn set_prompt(&mut self, prompt: &str) {
+        self.prompt = prompt.to_owned();
+    }
+
+    pub fn set_tokens(&mut self, tokens: u16) {
+        self.max_tokens = tokens
+    }
+
+    pub fn set_temperature(&mut self, temperature: u8) {
+        self.temperature = temperature;
+    }
+
+    pub fn set_completions(&mut self, completions: u8) {
+        self.n = completions;
+    }
+
+    pub fn set_echo(&mut self, echo: bool) {
+        self.echo = echo;
+    }
 }
 
 impl GPTResponse {
