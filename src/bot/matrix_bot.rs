056256ff4f4b6a0d37e7da1dd1d15871700597a2
Proudmuslim <proudmuslimos@protonmail.ch>
2021-09-13 13:33:33 -0700

    Refactor: Update `matrix-rust-sdk` to avoid CVE-2021-40823 and CVE-2021-40824 (https://matrix.org/blog/2021/09/13/vulnerability-disclosure-key-sharing)

--- src/bot/matrix_bot.rs
+++ src/bot/matrix_bot.rs
@@ -1,11 +1,10 @@
 use matrix_sdk::{
-    async_trait,
     room::Room,
     ruma::events::{
         room::message::{MessageEventContent, MessageType, TextMessageEventContent},
         AnyMessageEventContent, SyncMessageEvent,
     },
-    Client, ClientConfig, EventHandler, SyncSettings,
+    Client, ClientConfig, SyncSettings,
 };
 
 use crate::{bot::BotConfig, utils};
@@ -16,53 +15,44 @@ use url::Url;
 
 lazy_static! {
     static ref HTTP_CLIENT: reqwest::Client = reqwest::Client::new();
+    static ref CONFIG_FILE: BotConfig = confy::load("matrix-openai-bot").unwrap_or_else(|error| {
+        eprintln!("Error: {}", error);
+        exit(1)
+    });
 }
 
-pub struct OpenAIBot {
-    config: BotConfig,
-}
+async fn on_room_message(event: SyncMessageEvent<MessageEventContent>, room: Room) {
+    if let Room::Joined(room) = room {
+        let msg_body = if let SyncMessageEvent {
+            content:
+                MessageEventContent {
+                    msgtype: MessageType::Text(TextMessageEventContent { body: msg_body, .. }),
+                    ..
+                },
+            ..
+        } = event
+        {
+            msg_body
+        } else {
+            return;
+        };
 
-impl OpenAIBot {
-    pub fn new(config: BotConfig) -> Self {
-        Self { config }
-    }
-}
+        if msg_body.ends_with("--prompt") {
+            let prompt_vec: Vec<&str> = msg_body.split("--prompt").collect();
+            let prompt = prompt_vec[0].to_owned();
 
-#[async_trait]
-impl EventHandler for OpenAIBot {
-    async fn on_room_message(&self, room: Room, event: &SyncMessageEvent<MessageEventContent>) {
-        if let Room::Joined(room) = room {
-            let msg_body = if let SyncMessageEvent {
-                content:
-                    MessageEventContent {
-                        msgtype: MessageType::Text(TextMessageEventContent { body: msg_body, .. }),
-                        ..
-                    },
-                ..
-            } = event
-            {
-                msg_body
-            } else {
-                return;
-            };
+            let res = utils::get_response(&HTTP_CLIENT, prompt, &CONFIG_FILE).await;
 
-            if msg_body.ends_with("--prompt") {
-                let prompt_vec: Vec<&str> = msg_body.split("--prompt").collect();
-                let prompt = prompt_vec[0].to_owned();
+            let content = AnyMessageEventContent::RoomMessage(MessageEventContent::text_plain(
+                res.get_text()
+                    .replace("\\n", "\n")
+                    .replace("\\t", "\t")
+                    .as_str(),
+            ));
 
-                let res = utils::get_response(&HTTP_CLIENT, prompt, &self.config).await;
+            room.send(content, None).await.unwrap();
 
-                let content = AnyMessageEventContent::RoomMessage(MessageEventContent::text_plain(
-                    res.get_text()
-                        .replace("\\n", "\n")
-                        .replace("\\t", "\t")
-                        .as_str(),
-                ));
-
-                room.send(content, None).await.unwrap();
-
-                println!("Message sent");
-            }
+            println!("Message sent");
         }
     }
 }
@@ -71,12 +61,7 @@ pub async fn start() -> Result<(), matrix_sdk::Error> {
     let mut home = dirs::home_dir().expect("No home directory found!");
     home.push("matrix-openai-bot");
 
-    let config_file: BotConfig = confy::load("matrix-openai-bot").unwrap_or_else(|error| {
-        eprintln!("Error: {}", error);
-        exit(1)
-    });
-
-    for key in config_file.as_array() {
+    for key in CONFIG_FILE.as_array() {
         if key.is_empty() {
             eprintln!("Please ensure you have filled out the config file completely. Your configuration file should be located under the 'matrix-openai-bot' folder inside your config directory.");
             exit(1)
@@ -86,26 +71,24 @@ pub async fn start() -> Result<(), matrix_sdk::Error> {
     let client_config = ClientConfig::new().store_path(home);
 
     let homeserver_url =
-        Url::parse(config_file.homeserver()).expect("Couldn't parse homeserver URL.");
+        Url::parse(CONFIG_FILE.homeserver()).expect("Couldn't parse homeserver URL.");
 
     let client = Client::new_with_config(homeserver_url, client_config).unwrap();
 
     client
         .login(
-            config_file.username(),
-            config_file.password(),
+            CONFIG_FILE.username(),
+            CONFIG_FILE.password(),
             None,
             Some("OpenAI Bot"),
         )
         .await?;
 
-    println!("Logged in as: {}", &config_file.username());
+    println!("Logged in as: {}", &CONFIG_FILE.username());
 
     client.sync_once(SyncSettings::default()).await.unwrap();
 
-    client
-        .set_event_handler(Box::new(OpenAIBot::new(config_file)))
-        .await;
+    client.register_event_handler(on_room_message).await;
 
     let settings = SyncSettings::default().token(client.sync_token().await.unwrap());
     client.sync(settings).await;
